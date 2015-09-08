bc445286d2e00993f5c1b1dc3eddb583121f7d45
sme <sme>
2015-09-08 18:20:25 +0300

    Version 0.1

---
+++ js/docs-setup.js
@@ -0,0 +1,106 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "es.Services.Web",
+      "shortName": "es.Services.Web",
+      "type": "overview",
+      "moduleName": "es.Services.Web",
+      "shortDescription": "This module encapsulates the services, providers, factories and constants for the Entersoft AngularJS WEB API",
+      "keywords": "angularjs api constants encapsulates entersoft es factories module ngsanitize ngstorage overview providers services web"
+    },
+    {
+      "section": "api",
+      "id": "es.Services.Web.esStackTrace",
+      "shortName": "es.Services.Web.esStackTrace",
+      "type": "service",
+      "moduleName": "es.Services.Web",
+      "shortDescription": "esStackTrace",
+      "keywords": "api complete corresponding error es esstacktrace factory https javascript js library method object print printstacktrace provide reporting returns service services stack stacktrace stacktracejs trace web"
+    },
+    {
+      "section": "api",
+      "id": "es.Services.Web.esWebApi",
+      "shortName": "es.Services.Web.esWebApi",
+      "type": "service",
+      "moduleName": "es.Services.Web",
+      "shortDescription": "esWebApi",
+      "keywords": "$location $log $scope abcd accept acceptable access admin admindsdsds administrator alert angularjs ao api appear applicable application array assigned attention attribute autheticated authorization automatically based bearer boolean branch branchid browser browsers business cache call calling calls capable case cases category clean-up cleaned-up code codes comma company company-parameter-not-found companyparam complete config content-type contents corresponding count countof create credentials current data database default define defined definition depending descr description designer detailed developer development diagram dofetchpublicquery dologin dologout e-mail e-mails e1515a3c-8262-4581-8332-8663c2787964 ebs el-gr elements emprty empty entersoft equal err error es es_mail_bodyfooter esdaterange esgoperson esgrid esgridinfotokinfo esjanusgridlayout esmmstockitem esmmstockitem_def esparam esparams estype eswebapi eswebapiservice eswebgridinfo eswebgridlayout eswebuihelper example executed executing execution exist exists explicitly eyj0exaioijkv1qilcjhbgcioijiuzi1nij9 eyj1bmlxdwvfbmftzsi6imfkbwluiiwiec1lcy11c2vylxbhc3n3b3jkijoizw50zxjzb2z0iiwiec1lcy11c2vylwjyyw5jac1pzci6is6rzpjolyisingtzxmtdxnlci1syw5nlwlkijoizwwtr1iilcj4lwvzbg9naw5pbmzvlvn1ynnjcmlwdglvblbhc3n3b3jkijoicgfzc3gilcjpc3mioijfbnrlcnnvznqilcjhdwqioijodhrwoi8vbg9jywxob3n0iiwizxhwijoxndqxnzeznzg1lcjuymyioje0nde3mdc3odv9 false fcompanycode fcompanysitegid fetchcompanyparam fetchcompanyparams fetchpqinfo fetchpublicquery fetchpublicqueryinfo fieldname fieldvalue filter filterid footer form fpersoncodegid framework function fusergid general generic gid google greater gridex gridexlayout gridlayout group groupid handles headers hello help hold holds http httpverb inactive include info inputpage inputpagesize int internal janus json kendo-grid key kindsite kindwh langid layout length list local logged logged-in login login-invalid-user logout manually mapping mastertable mastertablename matter message messageid messages messaging method methods missing model myvalidparamkey nan natter net network neutral-abstract null num number object objects opensession operation operations options order pagesize paging param parameter parameters paramid params passwdkey password passx path pcompanyparam pcompanyparams pcompanyparamsvalue pcompanyparamvalue performed performs person personinterestcategoryvalue personlist pfilter pgroup place post powered pq pqfilterid pqgroupid pqoptions pqparams pqresult pre preference promise properties provider pseudo public query ready record records reflects regdate registered releated relogin rendering rep representation representing requested required requiring respect response result ret retrieve return returned returns rights rows sample samples scroller selection separated server service servicelevel services session set simplification site spa spas specific ssas status statustext storage stored string stringify strings structure subscriptionid subscriptionpassword success successful sucess suite supplied table template text time token total transformrequest transformresponse treated true type typical typically ui unauthorized undefined url user userid usermessage usersites valid values var verb web webapitoken webapitokenexpiresat wingridinfotoesgridinfo withcount world xxxx"
+    },
+    {
+      "section": "api",
+      "id": "es.Web.UI",
+      "shortName": "es.Web.UI",
+      "type": "overview",
+      "moduleName": "es.Web.UI",
+      "shortDescription": "This module encapsulates a set of directives, filters, services and methods for UI",
+      "keywords": "api directives encapsulates es filters methods module overview services set ui web"
+    },
+    {
+      "section": "api",
+      "id": "es.Web.UI.directive:esGrid",
+      "shortName": "esGrid",
+      "type": "directive",
+      "moduleName": "es.Web.UI",
+      "shortDescription": "This directive is responsible to render the html for the presentation of the results / data of an Entersoft Public Query.",
+      "keywords": "$log ae angular angularjs api application assign data directive element entersoft es esexecuteparams esfilterid esgrid esgridoptions esgroupid esuihelper eswebapi executing explicitly filter generates grid group html http instantiate kendo-grid object options order parameters params presentation provide public query render required responsible server services supplied telerik ui web"
+    },
+    {
+      "section": "api",
+      "id": "es.Web.UI.directive:esParam",
+      "shortName": "esParam",
+      "type": "directive",
+      "moduleName": "es.Web.UI",
+      "shortDescription": "esGrid esParam",
+      "keywords": "api directive div es esgrid esparam ui web"
+    },
+    {
+      "section": "api",
+      "id": "es.Web.UI.directive:esParamsPanel",
+      "shortName": "esParamsPanel",
+      "type": "directive",
+      "moduleName": "es.Web.UI",
+      "shortDescription": "Resize textarea automatically to the size of its text content.",
+      "keywords": "api automatically content directive div es resize size text textarea ui web"
+    },
+    {
+      "section": "api",
+      "id": "es.Web.UI.directive:esWebPq",
+      "shortName": "esWebPq",
+      "type": "directive",
+      "moduleName": "es.Web.UI",
+      "shortDescription": "esGrid esWebPq",
+      "keywords": "api directive div es esgrid eswebpq ui web"
+    },
+    {
+      "section": "api",
+      "id": "es.Web.UI.esUIHelper",
+      "shortName": "es.Web.UI.esUIHelper",
+      "type": "service",
+      "moduleName": "es.Web.UI",
+      "shortDescription": "esUIHelper",
+      "keywords": "addons api es esuihelper service ui web"
+    },
+    {
+      "section": "api",
+      "id": "es.Web.UI.filter:esTrustHtml",
+      "shortName": "esTrustHtml",
+      "type": "filter",
+      "moduleName": "es.Web.UI",
+      "shortDescription": "esGrid Directive",
+      "keywords": "api directive es esgrid filter ui web"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
