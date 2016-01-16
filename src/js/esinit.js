45fa70870c4bbf1ef72cf78fc5343b8857da3ac2
sme <sme>
2016-01-16 15:42:27 +0200

    '1.7.1'

--- src/js/esinit.js
+++ src/js/esinit.js
@@ -6,7 +6,7 @@
         return window._; //Underscore must already be loaded on the page 
     });
 
-    var version = "1.7.0";
+    var version = "1.7.1";
     var vParts = _.map(version.split("."), function(x) {
         return parseInt(x);
     });
