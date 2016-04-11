8e072968713300a0a917552ece0344ff32fe73c8
sme <sme>
2016-04-11 10:28:12 +0300

    advanced date range 2

--- dist/eswebapi.js
+++ dist/eswebapi.js
@@ -1,4 +1,4 @@
-/*! Entersoft Application Server WEB API - v1.8.5 - 2016-04-08
+/*! Entersoft Application Server WEB API - v1.8.5 - 2016-04-11
 * Copyright (c) 2016 Entersoft SA; Licensed Apache-2.0 */
 /***********************************
  * Entersoft SA
