6f362e4c8f6d228a67c0d6e8d793018c8c111946
sophie17Enter <sok@entersoft.gr>
2017-08-11 16:36:42 +0300

    inputs changed col-lg-3

--- examples/eswebapicalls/lib/eswebapi/dist/eswebapi.js
+++ examples/eswebapicalls/lib/eswebapi/dist/eswebapi.js
@@ -1,4 +1,4 @@
-/*! Entersoft Application Server WEB API - v1.21.0 - 2017-08-03
+/*! Entersoft Application Server WEB API - v1.21.0 - 2017-08-11
 * Copyright (c) 2017 Entersoft SA; Licensed Apache-2.0 */
 /***********************************
  * Entersoft SA
