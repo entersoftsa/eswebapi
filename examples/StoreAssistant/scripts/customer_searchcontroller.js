ea12285116a5cc5c9230ce551624ea53a93c5aee
sme <sme>
2017-10-23 10:20:09 +0300

    panel

--- examples/StoreAssistant/scripts/customer_searchcontroller.js
+++ examples/StoreAssistant/scripts/customer_searchcontroller.js
@@ -13,46 +13,16 @@
             var vm = this;
 
             vm.status = {
-                searchisOpen: true,
-                listisOpen: false,
-                formisOpen: false
+                listisOpen: true,
+                formisOpen: false,
+                xxx: true
             };
-            vm.searchDS = {};
-
-            var fields = new esWebUIHelper.ESParamsDefinitions();
-            fields.createDefinitions("Search for a customer", [{
-                    id: "Loyalty",
-                    aa: 1,
-                    caption: "Loyalty Number",
-                    visible: true,
-                },
-                {
-                    id: "eMail",
-                    aa: 3,
-                    caption: "eMail",
-                    visible: true,
-                },
-                {
-                    id: "Name",
-                    aa: 2,
-                    caption: "Name",
-                    visible: true,
-                },
-
-                {
-                    id: "Phone",
-                    aa: 4,
-                    caption: "Phone",
-                    visible: true,
-                    inputType: "tel"
-                }
-            ]);
-
-            vm.customerFields = fields;
-            vm.customerValues = fields.getParamsValues();
-
+                        
+            vm.myHide = function() {
+                vm.status.xxx = !vm.status.xxx;
+            }
             var pqOptions = new esGlobals.ESPQOptions(1, 10, true, true, false);
-            vm.pqDef = new esGlobals.ESPublicQueryDef("1", "ESWebManager", "BusEntitiesCustomers", pqOptions, vm.customerValues);
+            vm.pqDef = new esGlobals.ESPublicQueryDef("1", "ESWebManager", "BusEntitiesCustomers", pqOptions, new esGlobals.ESParamValues());
 
             vm.handleGridOptions = function(arg1) {
                 if (!arg1) {
@@ -64,28 +34,22 @@
                 arg1.filterable = false;
                 arg1.groupable = false;
                 arg1.change = function(e) {
+                    vm.status.xxx = false;
                     var selectedRows = e.sender.select();
                     if (selectedRows && selectedRows.length == 1) {
                         $timeout(function() {
                             vm.CustomerCode = e.sender.dataItem(selectedRows[0])["Code"];
-                            vm.status.formisOpen = true;
-                            vm.status.searchisOpen = false;
-                            vm.status.listisOpen = false;
+                            // vm.status.formisOpen = true;
+                            // vm.status.listisOpen = false;
+                            vm.status.xxx = false;
+
                         });
                         
                     }
                 }
                 return arg1;
             }
-                        
-            vm.searchCustomer = function() {
-                if (vm.searchDS && angular.isFunction(vm.searchDS.read)) {
-                    vm.searchDS.read();
-                    vm.status.listisOpen = true;
-                    vm.status.searchisOpen = false;
-                    vm.status.formisOpen = false;
-                }
-            }
+                       
         }
     ]);
 })();
