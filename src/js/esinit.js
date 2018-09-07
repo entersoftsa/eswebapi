4fe4ac22c72678e0a81815b56526ff87b7ad9c5e
sme <sme>
2018-09-07 17:12:46 +0300

    2.3.10

--- src/js/esinit.js
+++ src/js/esinit.js
@@ -6,7 +6,7 @@
         return window._; //Underscore must already be loaded on the page 
     });
 
-    var version = "2.3.9";
+    var version = "2.3.10";
     var vParts = _.map(version.split("."), function(x) {
         return parseInt(x);
     });
