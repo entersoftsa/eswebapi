ccd999e1e44cff7bba3f2589fb8304f594765776
sme <sme>
2019-04-18 02:36:26 +0300

    2.5.25

--- src/js/esinit.js
+++ src/js/esinit.js
@@ -6,7 +6,7 @@
 		return window._; //Underscore must already be loaded on the page 
 	});
 
-	var version = "2.5.24";
+	var version = "2.5.25";
 	var vParts = _.map(version.split("."), function (x) {
 		return parseInt(x);
 	});
