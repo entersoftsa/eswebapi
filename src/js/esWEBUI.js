60e4630d257f3b70a3f8e43c302918feedd9a133
Dimosthenis Stellakis <dimosstellakis@hotmail.com>
2019-11-25 14:43:54 +0200

    Timeline, minor changes

--- src/js/esWEBUI.js
+++ src/js/esWEBUI.js
@@ -1923,7 +1923,10 @@
 						};
 
 						$scope.toHeaderDateString = function (date) {
-							return kendo.toString(date, 'd MMM yyyy');
+							if (date instanceof Date)
+								return kendo.toString(date, 'd MMM yyyy');
+							else
+								return kendo.toString(new Date(date), 'd MMM yyyy');
 						};
 
 						var runDS = function () {
@@ -2018,6 +2021,7 @@
 											qualifier: { from: "Qualifier", type: "number" },
 											subtitle: { from: "SubTitle", type: "string" },
 											description: { from: "Body", type: "string" },
+											additionalInfo: { from: "AdditionalInfo", type: "string" },
 											formid: { from: "FormID", type: "string" },
 											fGID: { from: "fGID", type: "string" }
 										}
@@ -2043,7 +2047,10 @@
 								'	<div class="k-card-body">' +
 								'		# if (data.description) { #' +
 								'		<div class="k-card-description">' +
-								'			<p>#=data.description#</p>' +
+								'			<p>#= data.description #</p>' +
+								'			# if (data.additionalInfo) { #' +
+								'			<p class="text-muted">#= data.additionalInfo #</p>' +
+								'			# } #' +
 								'		</div>' +
 								'		# } #' +
 								'		# if (data.images && data.images.length > 0) { #' +
