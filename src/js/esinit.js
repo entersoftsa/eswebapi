a0f1feaca31bb32668e56cff770685476138ad30
sme <sme@entersoft.gr>
2022-09-01 13:18:16 +0300

    3.1.0

--- src/js/esinit.js
+++ src/js/esinit.js
@@ -6,7 +6,7 @@
         return window._; //Underscore must already be loaded on the page 
     });
 
-    var version = "3.0.14";
+    var version = "3.1.0";
     var vParts = _.map(version.split("."), function(x) {
         return parseInt(x);
     });
